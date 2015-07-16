9e088f7e73427623d89a29df4585c02ab5220ad6
Stakulik <stakulik@mail.ru>
2015-07-16 11:08:41 +0300

    Add check for uniqueness generating aliases

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,7 +2,7 @@ class LinksController < ApplicationController
 
   def create
     @link = Link.new(link_params)
-    @link.alias_link = create_alias_url
+    @link.alias_link = create_alias_link
     
     if @link.save
       $alias_link = @link.alias_link
@@ -19,15 +19,27 @@ class LinksController < ApplicationController
     params.require(:link).permit(:original_link, :created_at)
   end
 
+  def this_alias_link_exists?(alias_link)
+    /\A(.*)(#{request.host})\/{1}([A-Z0-9]{5})\z/.match alias_link
+    return Link.exists?(:alias_link => "#{$2}/#{$3}")
+  end
+
   def array_with_symbols
     symbArr = ('A'..'Z').to_a + (0..9).to_a.map(&:to_s)
     return symbArr
   end
 
-  def create_alias_url
-    alias_url = "#{request.host}/"
-    5.times { alias_url += array_with_symbols.sample }
-    return alias_url
+  def create_alias_link
+    alias_url = nil
+    
+    while alias_url == nil
+      alias_url = "#{request.host}/"
+      5.times { alias_url += array_with_symbols.sample }
+      alias_url = nil if this_alias_link_exists?(alias_url)
+    end
+
+    return alias_url 
   end
 
 end  
+
